51875777af38c2a9683b409e06a5e41e5eea58ed
jk <jinkh93@gmail.com>
2023-03-07 21:17:11 +0100

    update components

--- Sources/Eddy/Component.swift
+++ Sources/Eddy/Component.swift
@@ -5,7 +5,12 @@ public struct AppButton: View {
     let style: Font.TextStyle
     let role: ButtonRole?
     let action: () -> Void
-    public init(_ text: String, style: Font.TextStyle = .body, role: ButtonRole? = nil, action: @escaping () -> Void) {
+    public init(
+        _ text: String,
+        style: Font.TextStyle = .body,
+        role: ButtonRole? = nil,
+        action: @escaping () -> Void
+    ) {
         self.text = text
         self.style = style
         self.role = role
@@ -20,14 +25,18 @@ public struct AppButton: View {
 public struct CustomButton<Content>: View where Content: View {
     let role: ButtonRole?
     let action: () -> Void
-    let label: () -> Content
-    public init(role: ButtonRole? = nil, @ViewBuilder label: @escaping () -> Content, action: @escaping () -> Void) {
+    let label: Content
+    public init(
+        role: ButtonRole? = nil,
+        @ViewBuilder label labelBuilder: () -> Content,
+        action: @escaping () -> Void
+    ) {
         self.role = role
         self.action = action
-        self.label = label
+        self.label = labelBuilder()
     }
     public var body: some View {
-        Button(role: role, action: action, label: label)
+        Button(role: role, action: action, label: { label })
     }
 }
 
@@ -58,6 +67,9 @@ public struct HorizontalLeadingView<LeadingView>: View where LeadingView: View {
     public init(leadingView: LeadingView) {
         self.leadingView = leadingView
     }
+    public init(@ViewBuilder leading leadingBuilder: () -> LeadingView) {
+        self.leadingView = leadingBuilder()
+    }
     public var body: some View {
         HStack {
             leadingView
@@ -70,6 +82,9 @@ public struct HorizontalTrailingView<TrailingView>: View where TrailingView: Vie
     let trailingView: TrailingView
     public init(trailingView: TrailingView) {
         self.trailingView = trailingView
+    }
+    public init(@ViewBuilder trailing trailingBuilder: () -> TrailingView) {
+        self.trailingView = trailingBuilder()
     }
     public var body: some View {
         HStack {
@@ -85,10 +100,18 @@ where LeadingView: View, TrailingView: View {
     let trailingView: TrailingView
     public init(
         leading leadingView: LeadingView,
-        trailing trailingView: TrailingView) {
-            self.leadingView = leadingView
-            self.trailingView = trailingView
-        }
+        trailing trailingView: TrailingView
+    ) {
+        self.leadingView = leadingView
+        self.trailingView = trailingView
+    }
+    public init(
+        @ViewBuilder leading leadingBuilder: () -> LeadingView,
+        @ViewBuilder trailing trailingBuilder: () -> TrailingView
+    ) {
+        self.leadingView = leadingBuilder()
+        self.trailingView = trailingBuilder()
+    }
     public var body: some View {
         HStack {
             leadingView
@@ -101,59 +124,82 @@ where LeadingView: View, TrailingView: View {
 public typealias SectionText = AppText.Subheadline
 
 public struct AppSection<Content>: View where Content: View {
-    let content: () -> Content
-    public init(@ViewBuilder content: @escaping () -> Content) {
-        self.content = content
+    let content: Content
+    public init(@ViewBuilder content contentBuilder: () -> Content) {
+        self.content = contentBuilder()
     }
     public var body: some View {
-        Section(content: content)
+        Section(content: { content })
     }
 }
 
 public struct AppHeaderSection<Content>: View where Content: View {
     let header: SectionText
-    let content: () -> Content
+    let content: Content
     public init(
         key: String,
-        @ViewBuilder content: @escaping () -> Content
+        @ViewBuilder content contentBuilder: () -> Content
     ) {
         self.header = AppText.Subheadline(key: key)
-        self.content = content
+        self.content = contentBuilder()
+    }
+    public init(
+        _ text: String,
+        @ViewBuilder content contentBuilder: () -> Content
+    ) {
+        self.header = AppText.Subheadline(text)
+        self.content = contentBuilder()
     }
     public var body: some View {
-        Section(content: content, header: { header })
+        Section(content: { content }, header: { header })
     }
 }
 
 public struct AppFooterSection<Content>: View where Content: View {
     let footer: SectionText
-    let content: () -> Content
+    let content: Content
     public init(
         key: String,
-        @ViewBuilder content: @escaping () -> Content
+        @ViewBuilder content contentBuilder: () -> Content
     ) {
         self.footer = AppText.Subheadline(key: key)
-        self.content = content
+        self.content = contentBuilder()
+    }
+    public init(
+        _ text: String,
+        @ViewBuilder content contentBuilder: () -> Content
+    ) {
+        self.footer = AppText.Subheadline(text)
+        self.content = contentBuilder()
     }
     public var body: some View {
-        Section(content: content, footer: { footer })
+        Section(content: { content }, footer: { footer })
     }
 }
 
 public struct AppHeaderFooterSection<Content>: View where Content: View {
     let header: SectionText
     let footer: SectionText
-    let content: () -> Content
+    let content: Content
     public init(
         headerKey: String,
         footerKey: String,
-        @ViewBuilder content: @escaping () -> Content
+        @ViewBuilder content contentBuilder: () -> Content
     ) {
         self.header = AppText.Subheadline(key: headerKey)
         self.footer = AppText.Subheadline(key: footerKey)
-        self.content = content
+        self.content = contentBuilder()
+    }
+    public init(
+        headerText: String,
+        footerText: String,
+        @ViewBuilder content contentBuilder: () -> Content
+    ) {
+        self.header = AppText.Subheadline(headerText)
+        self.footer = AppText.Subheadline(footerText)
+        self.content = contentBuilder()
     }
     public var body: some View {
-        Section(content: content, header: { header }, footer: { footer })
+        Section(content: { content }, header: { header }, footer: { footer })
     }
 }
